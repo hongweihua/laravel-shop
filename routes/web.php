3a57e42bd96f9fb6a0e519834669fd90efca7ead
whhong <751997661@qq.com>
2018-12-27 09:09:51 +0000

    邮箱验证中间件

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', 'PageController@root')->name('root');
+Route::get('/', 'PagesController@root')->name('root');
+
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+
+    Route::group(['middleware' => 'email_verified'], function () {
+        Route::get('/test', function (){
+            return 'Your email is verified';
+        });
+    });
+});
 
 Auth::routes();
