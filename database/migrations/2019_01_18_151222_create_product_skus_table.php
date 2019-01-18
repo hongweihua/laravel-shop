08eb87b99a03d38b084f4c2fe22df2e9995da033
whhong <751997661@qq.com>
2019-01-18 16:01:33 +0800

    新增商品

--- database/migrations/2019_01_18_151222_create_product_skus_table.php
+++ database/migrations/2019_01_18_151222_create_product_skus_table.php
@@ -18,6 +18,7 @@ class CreateProductSkusTable extends Migration
             $table->string('title');
             $table->string('description');
             $table->unsignedInteger('stock');
+            $table->decimal('price', 10, 2);
             $table->unsignedInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
