325124c1c179445fea853e047926599682ce0021
whhong <751997661@qq.com>
2019-01-31 17:37:01 +0800

    add product to cart

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,12 @@ Route::group(['middleware' => 'auth'], function () {
         Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 
-
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 
-        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+        Route::post('cart', 'CartController@add')->name('cart.add');
+
 
 //        Route::get('/test', function (){
 //            return 'Your email is verified';
