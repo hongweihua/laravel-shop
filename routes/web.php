b3de6bbb935aebae8d400d88c77c70a0cbe4a030
whhong <751997661@qq.com>
2019-02-01 17:07:08 +0800

    create order

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,8 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('cart', 'CartController@index')->name('cart.index');
         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 
+        Route::post('orders', 'OrdersController@store')->name('orders.store');
+
 
 
 //        Route::get('/test', function (){
